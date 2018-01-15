089bafb0ce6b225646f80bdeee68d950338f4bcd
Alan Williams <awilliams@sis.tv>
2018-01-15 14:18:22 +0000

    Front end updates - include calculated VAT on form

--- src/js/apps/codingtest/expenses/expenses-controller.js
+++ src/js/apps/codingtest/expenses/expenses-controller.js
@@ -31,15 +31,46 @@ app.controller("ctrlExpenses", ["$rootScope", "$scope", "config", "restalchemy",
 		});
 	}
 
+	var calculateVat = function(val) {
+		var vat = "";
+		// If the value is not null
+		if (val != null) {
+			// Check to match a value and currency code
+            var regEx = val.match(/(^\d*[\.\,]?\d*)|([A-Za-z]{3})/g);
+            if (regEx.length > 0) {
+            	var currency = 'GBP';
+            	// Check the currency code
+            	if (regEx.length > 1) {
+            		currency = regEx[1];
+				}
+				// Only calculate VAT to display if we're working in GBP.
+				if (currency == 'GBP') {
+                    var amount = regEx[0]
+                    if (amount > 0) {
+                        vat = (amount * 0.2).toFixed(2);
+                    }
+				}
+            }
+		}
+		return vat;
+	}
+
 	$scope.saveExpense = function() {
 		if ($scope.expensesform.$valid) {
 			// Post the expense via REST
 			restExpenses.post($scope.newExpense).then(function() {
 				// Reload new expenses list
 				loadExpenses();
+			}).error(function(response) {
+				// No UX error message display element yet, but log to console for development.
+				console.log(response.message);
 			});
 		}
 	};
+
+	$scope.amountUpdate = function() {
+        $scope.newExpense.vat = calculateVat($scope.newExpense.amount);
+	}
 
 	$scope.clearExpense = function() {
 		$scope.newExpense = {};
